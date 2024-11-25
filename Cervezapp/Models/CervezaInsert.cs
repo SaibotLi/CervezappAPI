3f9c8f3a0296b5205f8e9bd411046e69aaa9e9b2
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-11-25 08:49:59 -0300

    Net 9.0 running and POST name + description now requires info

--- Cervezapp/Models/CervezaInsert.cs
+++ Cervezapp/Models/CervezaInsert.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Cervezapp;
 
 public class CervezaInsert
 {
+    [Required]
+    [MaxLength(20)]
 public string Name { get; set; } = string.Empty;
-
+    [Required]
+    [MaxLength(50)]
 public string Description {get; set;} = string.Empty;
 
 }
